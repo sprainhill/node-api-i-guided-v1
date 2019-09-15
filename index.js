51bf11c336d8d66192df7ac623cead26ef67a4e9
Rory Murray <rory.christian.murray@gmail.com>
2019-09-15 17:10:22 -0400

    Create POST endpoint

--- index.js
+++ index.js
@@ -8,24 +8,42 @@ const Hubs = require("./data/hubs-model"); // use hubs to get access to database
 
 const server = express();
 
+// add this line to teach express to parse JSON
+server.use(express.json());
+
 server.get("/", (req, res) => {
   res.send("hello web 20.75");
 });
 
 // see a list of hubs (channel)
 // returns a list of hubs
-server.get('/hubs', (req, res) => {
+server.get("/hubs", (req, res) => {
   // returns a promise
-  Hubs.find().then(hubs => {
-    // .json will convert the data passed to JSON
-    // also tells the client we're sending JSON through an HTTP header (content-type)
-    res.status(200).json(hubs);
-  }).catch(error => {
-    res.status(500).json({message 'error getting the list of hubs'});
-  });
+  Hubs.find()
+    .then(hubs => {
+      // .json will convert the data passed to JSON
+      // also tells the client we're sending JSON through an HTTP header (content-type)
+      res.status(200).json(hubs);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error getting the list of hubs" });
+    });
 });
 
 // create a hub
+server.post("/hubs", (req, res) => {
+  // http message is an object with headers and body => { headers: {}, body : { // data sent by client } }
+
+  const hubInfo = req.body;
+  console.log("hub info from body", hubInfo);
+  Hubs.add(hubInfo)
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error adding new hub" });
+    });
+});
 
 // delete a hub
 
